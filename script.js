4926988f9f799be92b7259a8b2ecfbf8343d64b3
hansana2012 <hansanagayesh708@gmail.com>
2026-04-19 18:38:35 +1100

    Add files via upload

--- script.js
+++ script.js
@@ -100,8 +100,8 @@ const videoData = [
   {
     title: "ඔයාට ගැලපෙන AI එක මොකක්ද?",
     desc: "What is the best AI for you?",
-    videoUrl: "https://www.youtube.com/embed/3JZ_D3ELwOQ?si=abc123",
-    videoId: "3JZ_D3ELwOQ",
+    videoUrl:  src="https://www.youtube.com/embed/E_5n3U8koE4?si=UZGcvLIVqACHsbF6",
+    videoId: "76WDNO06I-bUXzx6",
     thumbnailIcon: "fas fa-mobile-alt"
   },
   {
